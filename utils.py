b160d7f40328e1aefda20203b84c4eebb8df9108
Telegram Hizmetleri <84121542+LiderBeyim@users.noreply.github.com>
2025-05-13 22:28:26 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,15 @@
+from pyrogram.types import Message
+from configparser import ConfigParser
 
+config = ConfigParser()
+config.read("config.ini")
+OWNER_ID = int(config["bot"]["owner_id"])
+
+def is_admin(user_id: int, admins: list[int]):
+    return user_id == OWNER_ID or user_id in admins
+
+def get_flag():
+    return "🚩"
+
+def get_emoji():
+    return "🌟"
